e78951d9883f89ed7634ba8fc097572c0b128faa
Nandiehakuna <gakiikelly403@gmail.com>
2024-12-23 15:10:46 +0300

    cleanup: app.js

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { Button, StyleSheet } from 'react-native'
-import LoginScreen from './app/screens/LoginScreen';
-import { Text } from 'react-native';
-import Screen from './app/components/Screen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { DefaultTheme, NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from 'react-native'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
 
-// import {MaterialCommunityIcons} from '@icons/vector-icons'
-import AuthNavigator from './app/navigation/AuthNavigator';
 import AppNavigator from './app/navigation/AppNavigator';
-import MessageScreen from './app/screens/MessageScreen';
-import WecomeScreen from './app/screens/WecomeScreen';
 
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 
